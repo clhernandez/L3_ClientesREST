f34937bd2d90ecd2946404665b547996ac8e1b26
Claudio Hernández <claudio.hernandez.p@usach.cl>
2015-06-29 03:11:20 -0300

    Implementacion cliente golang & fixes

---
+++ clientes/goClient.go
@@ -0,0 +1,150 @@
+package main
+
+import "fmt"
+import "net/http"
+import "io/ioutil"
+import "os"
+import "encoding/json"
+
+type Server struct {
+	URL string `json:"url"`
+}
+
+type Trabajador struct {
+	Apellido string `json:"apellido"`
+	Cargo string `json:"cargo"`
+	Departamento string `json:"departamento"`
+	Email string `json:"email"`
+	FechaContratacion string `json:"fecha_contratacion"`
+	Nombre string `json:"nombre"`
+	Password string `json:"password"`
+	Rut string `json:"rut"`
+	Sueldo string `json:"sueldo"`
+	Telefono string `json:"telefono"`
+}
+
+func main() {
+
+	file, err := os.Open("server.json")
+	if err != nil {
+		fmt.Printf("%s", err)
+	}else{
+		data := make([]byte, 100)
+		count, err := file.Read(data)
+		if err != nil{
+			fmt.Printf("%s", err)	
+		}else{
+
+			fmt.Printf("read %d bytes: %q\n", count, data[:count])
+			json_url := data[:count]
+			var m Server
+
+			err := json.Unmarshal(json_url, &m)
+
+			if err != nil{
+				fmt.Printf("%s", err)	
+			}
+
+			url := m.URL
+			// fmt.Println("%s\n", url)
+			for{
+
+				fmt.Println("Cliente REST - RRHH/Trabajadores")
+				fmt.Println("<<MENU>>")
+				fmt.Println("1.- Listar todos los trabajadores")
+				fmt.Println("2.- Buscar un trabajador por RUT")
+				fmt.Println("3.- Salir")
+				fmt.Print("Ingrese Opcion: ")
+
+				
+				var in string
+				fmt.Scanln(&in)
+
+				if in=="1" {
+					fmt.Println("-------------------------------------------------------")
+					listarTrabajadores(url)
+				}else{
+					if in=="2"{
+						var rut string
+						fmt.Print("Ingrese RUT: ")
+						fmt.Scanln(&rut)
+						fmt.Println("-------------------------------------------------------")
+						buscarTrabajador(url, rut)
+					}else{
+						fmt.Println("<<FIN>>")
+						os.Exit(0)
+					}
+				}
+				
+				
+			}
+			
+		}
+	}
+}
+
+func buscarTrabajador(url string, rut string){
+	url += "rrhh/trabajadores/"+rut;
+	fmt.Println("Buscando Trabajador.")
+
+    resp, err := http.Get(url)
+    if err != nil {
+		fmt.Printf("%s", err)
+        os.Exit(1)
+	}else{		
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+	        fmt.Printf("%s", err)
+	        os.Exit(1)
+	    }else{
+		    var trabajador Trabajador
+
+	   		err := json.Unmarshal(body, &trabajador)
+
+	   		if err != nil{
+	   			fmt.Printf("%s", err)
+	   		}else{
+	   			fmt.Println("RUT: " + trabajador.Rut)
+	   			fmt.Println("Nombre: " + trabajador.Nombre)
+   				fmt.Println("Apellido: " + trabajador.Apellido)
+   				fmt.Println("Email: " + trabajador.Email)
+   				fmt.Println("-------------------------------------------------------")
+	   		}
+	    }
+	}
+}
+
+func listarTrabajadores(url string){
+	url += "rrhh/trabajadores";
+	fmt.Println("Consultando Listado Trabajadores.")
+
+    resp, err := http.Get(url)
+    if err != nil {
+		fmt.Printf("%s", err)
+        os.Exit(1)
+	}else{		
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+	        fmt.Printf("%s", err)
+	        os.Exit(1)
+	    }else{
+		    var trabajadores []Trabajador
+
+	   		err := json.Unmarshal(body, &trabajadores)
+
+	   		if err != nil{
+	   			fmt.Printf("%s", err)
+	   		}else{
+	   			for i := 0; i < len(trabajadores); i++ {
+	   				fmt.Println("RUT: " + trabajadores[i].Rut)
+	   				fmt.Println("Nombre: " + trabajadores[i].Nombre)
+	   				fmt.Println("Apellido: " + trabajadores[i].Apellido)
+	   				fmt.Println("Email: " + trabajadores[i].Email)
+	   				fmt.Println("-------------------------------------------------------")
+	   			}
+	   		}
+	    }
+	}
+}
---
+++ clientes/pyClient.py
@@ -0,0 +1,61 @@
+import json, sys, requests
+url_servicio = ""
+
+def listarTrabajadores():
+	try:
+		print "Consultando url: {0}".format(url_servicio)
+
+		r = requests.get(url_servicio)
+		if r.status_code == 200:
+			trabajadores = r.json();
+
+			print "Cantidad de trabajadores: {0}".format(len(trabajadores))
+
+			for trabajador in trabajadores:
+				print "-------------------------------------------------------"
+				print "RUT: {0}, Nombre: {0}, Apellido:{1}".format(trabajador["rut"], trabajador["nombre"], trabajador["apellido"])
+
+	except requests.exceptions.ConnectionError:
+		print "Error al conectarse con el servicio"
+
+def buscarTrabajador(rut):
+	try:
+		url_servicio_rut = url_servicio+"/"+rut
+		print "Consultando url: {0}".format(url_servicio_rut)
+		r2 = requests.get(url_servicio_rut);	
+		if r2.status_code==200:
+			trabajador_rut = r2.json()
+			print "-------------------------------------------------------"
+			print "RUT: {0}, Nombre: {0}, Apellido:{1}".format(trabajador_rut["rut"], trabajador_rut["nombre"], trabajador_rut["apellido"])
+
+	except requests.exceptions.ConnectionError:
+		print "Error al conectarse con el servicio"
+
+def main():
+	fin = False
+
+	while(fin==False):
+		print "Cliente REST - RRHH/Trabajadores"
+		print "<<MENU>>"
+		print "1.- Listar todos los trabajadores"
+		print "2.- Buscar un trabajador por RUT"
+		print "3.- Salir"
+		op = raw_input("Opcion: ")
+		if(op=="1"):
+			listarTrabajadores()
+		else:
+			if(op=="2"):
+				rut = raw_input("Rut Trabajador: ")
+				buscarTrabajador(rut)
+			else:
+				fin=True
+		print "-------------------------------------------------------"
+
+try:
+	f = open("server.json");
+	json = json.load(f)
+	url_servicio = json["url"]+"rrhh/trabajadores"
+	main()
+	print "<<FIN>>"
+except IOError:
+	print "El archivo {0} no existe.".format("server.json")
