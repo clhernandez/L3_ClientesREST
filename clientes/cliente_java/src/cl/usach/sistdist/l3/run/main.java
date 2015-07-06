b0976fc778d463b059313c92a00b6957c3737827
Claudio Hernández <claudio.hernandez.p@usach.cl>
2015-07-06 00:31:35 -0300

    Fix cliente java, implementar lectura de archivo (java 1.7)

--- clientes/cliente_java/src/cl/usach/sistdist/l3/run/main.java
+++ clientes/cliente_java/src/cl/usach/sistdist/l3/run/main.java
@@ -2,51 +2,85 @@ package cl.usach.sistdist.l3.run;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import cl.usach.sistdist.l3.vo.Trabajador;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class main {
 
 	public static void main(String[] args) {
-		URL url;
-		System.out.println("CLIENTE RRHH - REST");
-		System.out.println("<<MENU>>");
-		System.out.println("1.- Listar todos los trabajadores");
-		System.out.println("2.- Buscar un trabajador por RUT");
-		System.out.println("3.- Salir");
-		Scanner s = new Scanner(System.in);
-		String opc = s.next();
 		
-		switch (opc) {
-		case "1":
-			listarTrabajadores();
-			break;
+		//Leer archivo de configuracion.
+		Gson gson = new Gson();//parser json
+		Path path = FileSystems.getDefault().getPath("", "server.json");
+		try {
+			List<String> file = Files.readAllLines(path , StandardCharsets.UTF_8);
+			Type stringToMap = new TypeToken<Map<String, String>>(){}.getType();
+			Map<String, String> map = gson.fromJson(file.get(0), stringToMap);
 			
-		case "2":
-			System.out.print("Ingrese Rut del trabajador: ");
-			s = new Scanner(System.in);
-			String rut = s.next();
-			System.out.println(rut);
-			buscartrabajador(rut);
-			break;
-			
-		default:
-			System.out.println("<<SALIR>>");
-			System.exit(0);
-			break;
+			if(map.get("url")!=null){
+				boolean fin = false;
+				String url = map.get("url");
+				while(fin==false){
+				System.out.println("CLIENTE RRHH - REST");
+				System.out.println("<<MENU>>");
+				System.out.println("1.- Listar todos los trabajadores");
+				System.out.println("2.- Buscar un trabajador por RUT");
+				System.out.println("3.- Salir");
+				System.out.print("Ingrese una opcion: ");
+				Scanner s = new Scanner(System.in);
+				String opc = s.next();
+				
+				
+					switch (opc) {
+					case "1":
+						System.out.println("-------------------------------------------------------");
+						listarTrabajadores(url);
+						break;
+						
+					case "2":
+						System.out.print("Ingrese Rut del trabajador: ");
+						s = new Scanner(System.in);
+						String rut = s.next();
+						System.out.println(rut);
+						System.out.println("-------------------------------------------------------");
+						buscartrabajador(url, rut);
+						break;
+						
+					default:
+						System.out.println("<<SALIR>>");
+						System.exit(0);
+						break;
+					}	
+					System.out.println("-------------------------------------------------------");
+				}
+				
+			}else{
+				System.out.println("Error en el archivo de servicios.");
+			}
+		} catch (IOException e) {
+			System.out.println("No se encuentra el archivo servers.json");
 		}
+
 	}
 	
-	private static void listarTrabajadores(){
+	private static void listarTrabajadores(String url){
 		try {
-			URL url = new URL("http://45.55.214.35:3001/rrhh/trabajadores/");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			URL url_trabajadores = new URL(url + "rrhh/trabajadores/");
+			HttpURLConnection conn = (HttpURLConnection) url_trabajadores.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
 			
@@ -84,13 +118,13 @@ public class main {
 		}
 	}
 	
-	private static void buscartrabajador(String rut){
+	private static void buscartrabajador(String url, String rut){
 		try {
 			//^(\d+)-([0-9]|k|K) expresion regular para validar rut (formato 11111111-1)
 			
 			
-			URL url = new URL("http://45.55.214.35:3001/rrhh/trabajadores/"+rut);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			URL url_buscar_rut = new URL(url + "rrhh/trabajadores/"+rut);
+			HttpURLConnection conn = (HttpURLConnection) url_buscar_rut.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
 			
